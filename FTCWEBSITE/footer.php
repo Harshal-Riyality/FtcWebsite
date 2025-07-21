60235690f1f6e36d89faae53d7c7a686ab305bc3
Harshal_Riyality <harshalnirali.riyality@gmail.com>
2025-07-21 12:07:43 +0530

    collage project

---
+++ FTCWEBSITE/footer.php
@@ -0,0 +1,136 @@
+<!-- Main Footer Section Start -->
+    <footer class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- Footer Header Start -->
+                    <div class="footer-header">
+                        <!-- Footer Logo Start -->
+                        <div class="footer-logo">
+                            <img src="images\FTC-images\FTC-logo.png" alt="" height="60px" width="60px">
+                        </div>
+                        <!-- Footer Logo End -->
+                        
+                        <!-- Footer Social Links Start -->
+                        <div class="footer-social-links">
+                            <ul>
+                                <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- Footer Social Links End -->
+                    </div>
+                    <!-- Footer Header End -->
+                </div>
+
+               
+
+                <div class="col-lg-4 col-md-4 col-6">
+                    <!-- Footer Links Start -->
+                    <div class="footer-links footer-quick-links">
+                        <h3>Useful Links</h3>
+                        <ul>
+                            <li><a href="index.html">AICTE</a></li>
+                            <li><a href="about.html">DTE</a></li>
+                            <li><a href="services.html">BATU University</a></li>
+                            <li><a href="blog-single.html">Social Welfare</a></li>
+                            <li><a href="blog-single.html">AICTE Feedback</a></li>
+                            <li><a href="blog-single.html">Grievance</a></li>
+
+                        </ul>
+                    </div>
+                    <!-- Footer Links End -->
+                </div>
+                
+                <div class="col-lg-4 col-md-4 col-6">
+                    <!-- Footer Links Start -->
+                    <div class="footer-links">
+                        <h3>Useful Links</h3>
+                        <ul>
+                            <li><a href="service-single.html">MahaDBT</a></li>
+                            <li><a href="service-single.html">Govt of India</a></li>
+                            <li><a href="service-single.html">Govt of Maharashtra</a></li>
+                            <li><a href="service-single.html">National Scholarship Portal</a></li>
+                            <li><a href="service-single.html">NPTEL</a></li>
+                            <li><a href="service-single.html">Certificate Of E Waste Recycling</a></li>
+
+                        </ul>
+                    </div>
+                    <!-- Footer Links End -->
+                </div>
+                
+                <div class="col-lg-4 col-md-4 col-6">
+                    <!-- Footer Links Start -->
+                    <div class="footer-links">
+                        <h3>Contact Us</h3>
+                        <p>Fabtech Education Society’s
+                            Fabtech Technical Campus College of Engineering & Research</p>
+                         <p>Gate No. 565/1, Pandharpur Road, Sangola, Dist - Solapur - 413307</p>
+                        
+                        <ul>
+                            <li><a href="#">+91 8408 888 657</a></li>
+                            <li><a href="#"> info@ftccoe.ac.in </a></li>
+                            <li><a href="#"> www.ftccoe.ac.in</a></li>
+                        </ul>
+                    </div>
+                    <!-- Footer Links End -->
+                </div>
+                
+               
+            </div>
+        </div>
+
+        <!-- Footer Copyright Start -->
+        <div class="footer-copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- Footer Copyright Text Start -->
+                        <div class="footer-copyright-text">
+                            <p>Copyright © 2025 Fabtech Technical Campus College of Engineering & Research All Rights Reserved. Designed and Developed by <a href="https://riyalitysoftwares.com/" target="_blank">Riyality Softwares</a></p>
+</p>
+                        </div>
+                        <!-- Footer Copyright Text End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer Copyright End -->
+    </footer>
+    <!-- Main Footer Section End -->
+    
+    <!-- Jquery Library File -->
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <!-- Bootstrap js file -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Validator js file -->
+    <script src="js/validator.min.js"></script>
+    <!-- SlickNav js file -->
+    <script src="js/jquery.slicknav.js"></script>
+    <!-- Swiper js file -->
+    <script src="js/swiper-bundle.min.js"></script>
+    <!-- Counter js file -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <!-- Magnific js file -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- SmoothScroll -->
+    <script src="js/SmoothScroll.js"></script>
+    <!-- Parallax js -->
+    <script src="js/parallaxie.js"></script>
+    <!-- MagicCursor js file -->
+    <script src="js/gsap.min.js"></script>
+    <script src="js/magiccursor.js"></script>
+    <!-- Text Effect js file -->
+    <script src="js/SplitText.js"></script>
+    <script src="js/ScrollTrigger.min.js"></script>
+    <!-- YTPlayer js File -->
+    <script src="js/jquery.mb.YTPlayer.min.js"></script>
+    <!-- Wow js file -->
+    <script src="js/wow.min.js"></script>
+    <!-- Main Custom js file -->
+    <script src="js/function.js"></script>
+    <script src="../../demo.awaikenthemes.com/assets/js/theme-panel.js"></script>
+</body>
+</html>
---
+++ FTCWEBSITE/header.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="Awaiken">
+	<!-- Page Title -->
+    <title>Fabtech Technical College</title>
+	<!-- Favicon Icon -->
+	<link rel="shortcut icon" type="image/x-icon" href="images\FTC-images\FTC-logo.png">
+	<!-- Google Fonts Css-->
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&amp;display=swap" rel="stylesheet">
+	<!-- Bootstrap Css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!-- SlickNav Css -->
+	<link href="css/slicknav.min.css" rel="stylesheet">
+	<!-- Swiper Css -->
+	<link rel="stylesheet" href="css/swiper-bundle.min.css">
+	<!-- Font Awesome Icon Css-->
+	<link href="css/all.min.css" rel="stylesheet" media="screen">
+	<!-- Animated Css -->
+	<link href="css/animate.css" rel="stylesheet">
+    <!-- Magnific Popup Core Css File -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<!-- Mouse Cursor Css File -->
+	<link rel="stylesheet" href="css/mousecursor.css">
+	<!-- Main Custom Css -->
+	<link href="css/custom.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+    <!-- Preloader Start -->
+	<div class="preloader">
+		<div class="loading-container">
+			<div class="loading"></div>
+			<div id="loading-icon"><img src="images\FTC-images\FTC-logo.png" alt=""></div>
+		</div>
+	</div>
+	<!-- Preloader End -->
+
+    <!-- Topbar Section Start -->
+    <div class="topbar">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-md-4">
+                    <div class="topbar-info-text">
+                        <p style="font-family: emoji;">Fabtech Education Society’s <br>
+Fabtech Technical Campus College of Engineering & Research</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="topbar-info-text">
+                        <p style="font-family: emoji;">An Autonomous Institute <br>
+Affiliated to Dr.Babasaheb Ambedkar Technological University(BATU), Lonere. <br>
+Approved by AICTE, New Delhi, Recognised by Govt.Of Maharashtra, DTE Mumbai. <br>
+Accredited by NAAC, Bangalore, ISO 9001:2005 Certified</p>
+                    </div>
+                </div>
+
+                <div class="col-md-2">
+                    <div class="topbar-info-text">
+                        <!-- Header Btn Start -->
+                        <div class="header-btn">
+                            <h6 class="btn btn-secondary" style="font-size: 15px;">INSTITUTION CODE 6756</h6>
+                        </div>
+                        <!-- Header Btn End -->
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+    <!-- Topbar Section End -->
+
+    <!-- Header Start -->
+	<header class="main-header">
+		<div class="header-sticky">
+			<nav class="navbar navbar-expand-lg">
+				<div class="container">
+					<!-- Logo Start -->
+					<a class="navbar-brand" href="index-2.html">
+						<img src="images\FTC-images\FTC-logo.png" alt="Logo" height="60PX" width="60PX">
+					</a>
+					<!-- Logo End -->
+
+					<!-- Main Menu Start -->
+					<div class="collapse navbar-collapse main-menu">
+                        <div class="nav-menu-wrapper">
+                            <ul class="navbar-nav mr-auto" id="menu">
+                                <li class="nav-item"><a class="nav-link" href="">HOME</a></li>  
+                                <li class="nav-item submenu"><a class="nav-link" href="#">ABOUT US</a>
+                                    <ul>                                        
+                                        <li class="nav-item"><a class="nav-link" href="#">ABOUT CONFERENCE</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">ABOUT FTC</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">CHIEF PATRONS</a></li>
+                                    </ul>
+                                </li>                              
+                                <li class="nav-item"><a class="nav-link" href="">KEY NOTE SPEAKERS</a>
+                                <li class="nav-item"><a class="nav-link" href="">COMMITTEE</a> 
+                                <li class="nav-item submenu"><a class="nav-link" href="#">FOR AUTHERS</a>
+                                    <ul>                                        
+                                        <li class="nav-item"><a class="nav-link" href="#">CALL FOR AUTHERS</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">INSTRUCTIONS FOR AUTHERS</a></li>
+                                    </ul>
+                                </li> 
+                                <li class="nav-item"><a class="nav-link" href="">IMPORTANT DATES</a></li>
+                                <li class="nav-item"><a class="nav-link" href="">VENUE</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">CONTACT US</a></li>                         
+                            </ul>
+                        </div>
+
+                        
+					</div>
+					<!-- Main Menu End -->
+					<div class="navbar-toggle"></div>
+				</div>
+			</nav>
+			<div class="responsive-menu"></div>
+		</div>
+	</header>
+	<!-- Header End -->
